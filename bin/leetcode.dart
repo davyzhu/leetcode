7ca5289a0cbabac78caa16e6dd5df9c2c676cd20
Zhu, Shenli <zhu_shenli@outlook.com>
2023-09-30 16:52:26 +0800

    1766 logic pass, but fail at time limit

--- bin/leetcode.dart
+++ bin/leetcode.dart
@@ -1,10 +1,34 @@
-import 'dart:collection';
-
 void main() {
-  var s = 'leetcode';
-  var s2 = s.split('').map((e) => StringBuffer(e));
-  var q = Queue.of(s2);
-  for (var e in q) {
-    print(e.runtimeType);
+  createCoprime();
+}
+
+void createCoprime() {
+  final sw = Stopwatch()..start();
+  var coprime = List<List<bool>>.empty(growable: true);
+  for (var i = 1; i <= 50; i++) {
+    coprime.add([]);
+    for (var j = 1; j <= 50; j++) {
+      if (i.gcd(j) == 1) {
+        coprime[i - 1].add(true);
+      } else {
+        coprime[i - 1].add(false);
+      }
+    }
   }
+  sw.stop();
+  print(sw.elapsedMicroseconds);
+  //print(coprime);
+}
+
+void createNotCoprime() {
+  var notCoprimeSet = List<Set<int>?>.empty(growable: true);
+  for (var i = 1; i <= 50; i++) {
+    notCoprimeSet.add({});
+    for (var j = i; j <= 50; j++) {
+      if (i.gcd(j) != 1) {
+        notCoprimeSet[i - 1]!.add(j);
+      }
+    }
+  }
+  print(notCoprimeSet);
 }
